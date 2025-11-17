d016a7383d3125b73181dbd774572d9f14b1f8f8
khushi563 <220403@kit.ac.in>
2025-11-17 13:46:12 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api=axios.create({
-  baseURL:'https://aashray-backend-h535.onrender.com',
+  baseURL:process.env.REACT_APP_API_URL,
   headers:{'Content-Type':'application/json'}
 });
 api.interceptors.request.use(c=>{const t=localStorage.getItem('ngo_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c;});
